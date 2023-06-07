b6ac54212250bf7122c8192dd1859882ca787c52
Dominic Bor <dominicbor44@.com>
2023-06-07 12:03:23 -0700

    script to print the absolute path

---
+++ 0x00-shell_basics/0-current_working_directory.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Get the current working directory
+current_directory=$(pwd)
+
+# Print the absolute path of the current working directory
+echo "The absolute path of the current working directory is: $current_directory"
+
